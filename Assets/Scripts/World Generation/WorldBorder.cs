1b72e60ae39aab7827ab1cf9882073a031ff2958
Limphus12 <Limphus12@gmail.com>
2022-12-03 22:57:28 +0000

    03/12/2022 - Update 002
    we can now throw the player across the world after passing through a world border! pog

--- Assets/Scripts/World Generation/WorldBorder.cs
+++ Assets/Scripts/World Generation/WorldBorder.cs
@@ -5,23 +5,76 @@ using UnityEngine.Events;
 
 namespace com.limphus.retro_survival_shooter
 {
+    public enum CardinalDirection { NORTH, EAST, SOUTH, WEST }
+
     public class WorldBorder : MonoBehaviour
     {
-        [SerializeField] private UnityEvent unityEvent;
+        [SerializeField] private CardinalDirection direction;
 
-        private bool check = false;
+        [SerializeField] private UnityEvent unityEvent;
 
         private void OnTriggerEnter(Collider other)
         {
             //if the player enters this trigger
-            if (other.CompareTag("Player") && !check)
+            if (other.CompareTag("Player"))
             {
-                //added this check to hopefully stop an inf loop.
-                check = true;
-
                 //call our event
                 unityEvent.Invoke();
+
+                CheckDirection(other);
             }
+        }
+
+        private void CheckDirection(Collider other)
+        {
+            Vector3 currentPos = other.transform.position;
+            Vector3 newPos;
+
+            float heightOffset = 2f;
+
+            //calculate the new position of teh player!
+            switch (direction)
+            {
+                case CardinalDirection.NORTH:
+
+                    newPos = new Vector3(currentPos.x, currentPos.y + heightOffset, 2);
+
+                    MovePlayer(other.transform, newPos);
+
+                    break;
+
+                case CardinalDirection.EAST:
+
+                    newPos = new Vector3(2, currentPos.y + heightOffset, currentPos.z);
+
+                    MovePlayer(other.transform, newPos);
+
+                    break;
+
+                case CardinalDirection.SOUTH:
+
+                    newPos = new Vector3(currentPos.x, currentPos.y + heightOffset, 510);
+
+                    MovePlayer(other.transform, newPos);
+
+                    break;
+
+                case CardinalDirection.WEST:
+
+                    newPos = new Vector3(510, currentPos.y + heightOffset, currentPos.z);
+
+                    MovePlayer(other.transform, newPos);
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void MovePlayer(Transform other, Vector3 position)
+        {
+            other.position = position;
         }
     }
 }
